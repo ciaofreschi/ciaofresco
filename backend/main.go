fd7066fd3a3cd676ff8c4dee01a61f614e3e0b4c
Matteo Bucci <matteo.bucci.1995@gmail.com>
2023-11-16 22:59:11 +0100

    feat: Prof of concept of a GO backend with Dockerfile

--- backend/main.go
+++ backend/main.go
@@ -12,6 +12,7 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "This is my website!\n")
 }
+
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
